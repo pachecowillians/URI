c131604d96c154d457cf2b24c3bd4673a02e0dfc
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-24 16:38:04 -0300

    Solving problem 3145

---
+++ 3145.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <stack> //pilha
+#include <queue> //fila
+#include <functional>
+#include <tr1/functional> //hash
+#include <vector>
+#include <cstring>
+#include <string>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    float n, x;
+    cin >> n >> x;
+    cout << fixed << setprecision(2) << x/ (n + 2) << endl;
+    return 0;
+}

20b658f756fa5884968af29313b180ff262de29f
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-26 09:04:02 -0300

    Solving problem 3157

---
+++ 3157.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <stack> //pilha
+#include <queue> //fila
+#include <functional>
+#include <tr1/functional> //hash
+#include <vector>
+#include <cstring>
+#include <string>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << "Nao se refuta ciencia com opiniao" << endl;
+    return 0;
+}

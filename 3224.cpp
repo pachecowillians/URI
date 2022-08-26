615a6c5d626dec3af63b0ed9c8bcd9b7c6d89df0
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-26 11:23:14 -0300

    Solving problem 3224

---
+++ 3224.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <stack> //pilha
+#include <queue> //fila
+#include <functional>
+#include <tr1/functional> //hash
+#include <vector>
+#include <cstring>
+#include <string>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    string m, p;
+    cin >> m >> p;
+    if (m.length() >= p.length())
+    {
+        cout << "go" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+
+    return 0;
+}

797beb4282a43076902f425d9ab84911b217eea5
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-26 21:49:54 -0300

    Indenting code

--- 2457.py
+++ 2457.py
@@ -1,21 +1,23 @@
-def VerificaPalavraLetra(palavra,letra):
+def VerificaPalavraLetra(palavra, letra):
     for x in range(len(palavra)):
-        if(palavra[x] == letra):
+        if (palavra[x] == letra):
             return 1
     return 0
 
-def ContaPalavrasComLetra(lista,letra):
+
+def ContaPalavrasComLetra(lista, letra):
     contPalavras = 0
     contPalavrasComLetra = 0
 
     for c in range(len(lista)):
-        contPalavrasComLetra += VerificaPalavraLetra(lista[c],letra)
-        contPalavras+=1
+        contPalavrasComLetra += VerificaPalavraLetra(lista[c], letra)
+        contPalavras += 1
 
     return (contPalavrasComLetra/contPalavras)*100
+
 
 letra = str(input())
 letra = letra[0]
 frase = input().split(" ")
 
-print("{:.1f}".format(ContaPalavrasComLetra(frase,letra)))
+print("{:.1f}".format(ContaPalavrasComLetra(frase, letra)))
--- 2867.py
+++ 2867.py
@@ -1,5 +1,5 @@
 ct = int(input())
 for x in range(ct):
-    n,m = input().split(' ')
+    n, m = input().split(' ')
     s = str(int(n)**int(m))
     print(len(s))

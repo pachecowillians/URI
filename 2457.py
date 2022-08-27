3951cadc97d243514f29e58bfa376ee6d3aa2f7a
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-08-26 21:48:47 -0300

    Solving problem 2457

---
+++ 2457.py
@@ -0,0 +1,21 @@
+def VerificaPalavraLetra(palavra,letra):
+    for x in range(len(palavra)):
+        if(palavra[x] == letra):
+            return 1
+    return 0
+
+def ContaPalavrasComLetra(lista,letra):
+    contPalavras = 0
+    contPalavrasComLetra = 0
+
+    for c in range(len(lista)):
+        contPalavrasComLetra += VerificaPalavraLetra(lista[c],letra)
+        contPalavras+=1
+
+    return (contPalavrasComLetra/contPalavras)*100
+
+letra = str(input())
+letra = letra[0]
+frase = input().split(" ")
+
+print("{:.1f}".format(ContaPalavrasComLetra(frase,letra)))
